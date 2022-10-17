2c71d08cbf9ce795c4ca3a0835d82c3125e761bf
jaeheonkim0913 <jaeheonkim0913@gmail.com>
2022-10-16 22:48:36 -0400

    a5 Javascript sp22

---
+++ public/tuiter/WhoToFollowList/index.js
@@ -0,0 +1,15 @@
+import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import people from "./who.js";
+
+const WhoToFollowList = () => {
+    return `
+      <ul class="list-group">
+         ${people
+        .map((who) => {
+            return WhoToFollowListItem(who);
+        })
+        .join("")}
+      </ul>
+   `;
+};
+export default WhoToFollowList;
