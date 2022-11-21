6d215f58525c638870ff058e5720569c86f8762a
jaeheonkim0913 <jaeheonkim0913@gmail.com>
2022-11-21 12:19:19 -0500

    fix

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -14,6 +14,7 @@ export const findTuits = async () => {
     return tuits;
 };
 
+
 export const deleteTuit = async (tid) => {
     const response = await axios.delete(`${TUITS_API}/${tid}`);
     return response.data;
